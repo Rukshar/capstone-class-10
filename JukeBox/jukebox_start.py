1740321d2a1456364ca05ebacf06588321efc2de
geert@xomnia.com <pukcon-2tyNfi-budtyj>
2019-03-15 15:20:45 +0100

    added docstrings

--- JukeBox/jukebox_start.py
+++ JukeBox/jukebox_start.py
@@ -8,28 +8,43 @@ from JukeBox.jukebox_functions import setup_new_round, play_next_song
 from apscheduler.schedulers.background import BlockingScheduler
 
 music_folder = '../music/'
+db = '../db/dev.db'
 
-engine = create_engine('sqlite:///../db/dev.db', echo=False)
-Session = sessionmaker(bind=engine)
-session = Session()
-# note: removed session.configure and put bind in sessionmaker
-# todo: after few rounds error in database threads
 
-# initiate database
-if not os.path.isfile('../db/dev.db'):
-    print('No existing database found, starting new session')
-    Base.metadata.create_all(engine)
+def start_jukebox(music_folder=music_folder, db_path=db):
+    """
+    :param music_folder: string, path to music
+    :param db_path: string, path to store database
+    :return: None
+    """
 
-populate(session, music_folder)
+    db = 'sqlite:///{}'.format(db_path)
 
-round_end = setup_new_round(session, first_round=True)
-first_round = datetime.now() + timedelta(minutes=0, seconds=1)
-scheduler = BlockingScheduler()
+    engine = create_engine(db, echo=False)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# first songs starts after first round of voting (1 minute)
-scheduler.add_job(play_next_song, 'date', run_date=first_round, args=[Session, scheduler, music_folder])
-print('Starting Jukebox')
-scheduler.start()
+    # initiate database
+    if not os.path.isfile('../db/dev.db'):
+        print('No existing database found, starting new session')
+        Base.metadata.create_all(engine)
+
+    populate(session, music_folder)
+
+    round_end = setup_new_round(session, first_round=True)
+    first_round = datetime.now() + timedelta(minutes=0, seconds=1)
+    scheduler = BlockingScheduler()
+
+    # first songs starts after first round of voting (1 minute)
+    scheduler.add_job(play_next_song, 'date', run_date=first_round, args=[Session, scheduler, music_folder])
+    print('Starting Jukebox')
+    scheduler.start()
+
+    return None
+
+
+if __name__ == '__main__':
+    start_jukebox()
 
 
 
