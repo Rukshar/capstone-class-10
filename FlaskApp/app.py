785e79fc5926a5ddfd4d848dd77d53650815ae8d
Bradley de Leeuw <brdeleeuw@gmail.com>
2019-03-22 12:13:38 +0100

    remove imports that are no longer used

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from flask import Flask, request, render_template, flash, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import and_
-from db.objects import Base, Songs, Votes, Round, User, SelectedSongs, IPAddress
+from db.objects import Songs, Votes, Round, SelectedSongs, IPAddress
 
 app = Flask(__name__)
 app.config.from_pyfile('.secrets')
