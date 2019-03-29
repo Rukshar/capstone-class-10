eb4980677ecec4e0cb449edb5c8773ff3f413e6d
Bradley de Leeuw <brdeleeuw@gmail.com>
2019-03-29 16:47:59 +0100

    instantiate jukebox object due to refactoring of jukebox_functions

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
 from threading import Thread
-from src.jukebox.jukebox_functions import start_jukebox
+from src.jukebox.jukebox_functions import JukeBox
 from src.flaskapp.app import app
 
 music_folder = 'music/'
 db_path = 'src/db/dev.db'
 
+jukebox = JukeBox(music_folder, db_path)
+
 if __name__ == '__main__':
-    jukebox_args = {"music_folder": music_folder, "db_path": db_path}
     flaskapp_args = {"debug": False,"host": "0.0.0.0","port": 5000}
 
-    jukebox_thread = Thread(target=start_jukebox, kwargs=jukebox_args)
+    jukebox_thread = Thread(target=jukebox.start_jukebox)
     flaskapp_thread = Thread(target=app.run, kwargs=flaskapp_args)
 
     jukebox_thread.start()
