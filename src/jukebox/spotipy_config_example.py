91f62486e837c3eaa08a732f8c539d9f822c4c6b
geertklop@gmail.com <pukcon-2tyNfi-budtyj>
2019-04-23 13:34:11 +0200

    change in spotipy config example redirect URI (no ports needed)

--- src/jukebox/spotipy_config_example.py
+++ src/jukebox/spotipy_config_example.py
@@ -3,4 +3,4 @@ CLIENT_SECRET='your-spotify-client-secret'
 USERNAME = "your-spotify-username" # spotify username
 TARGET_PLAYLIST_URI = "target-playlist-uri" # the playlist you want as target, you can initialize an empty playlist
 SOURCE_PLAYLIST_URI = "source-playlist-uri" # the playlist you want to pull songs from
-REDIRECT_URI='your-redirect-uri' # e.g. localhost:5000
+REDIRECT_URI='your-redirect-uri' # e.g. 'http://localhost/'
