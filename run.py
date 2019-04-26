3117585ac921494c95333bbec8e7bb9af7fc34d0
Bradley de Leeuw <brdeleeuw@gmail.com>
2019-04-26 18:14:21 +0200

    add run.py to resolve merge conflicts

---
+++ run.py
@@ -0,0 +1,17 @@
+from threading import Thread
+from src.jukebox.jukebox_functions import JukeBox
+from src.flaskapp.app import app
+
+music_folder = 'music/'
+db_path = 'postgresql://postgres:docker@localhost:5432/postgres'
+
+jukebox = JukeBox(music_folder, db_path, None)
+
+if __name__ == '__main__':
+    flaskapp_args = {"debug": False,"host": "0.0.0.0","port": 5000}
+
+    jukebox_thread = Thread(target=jukebox.start_jukebox)
+    flaskapp_thread = Thread(target=app.run, kwargs=flaskapp_args)
+
+    jukebox_thread.start()
+    flaskapp_thread.start()
